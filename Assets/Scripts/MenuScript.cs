544778cab27a35058078a3b03d3b6e156729f53f
Val Vinuya <valvinuya@gmail.com>
2016-09-09 08:41:19 +0800

    no message

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour {
+
+	public void loadLevel(string scene){
+		SceneManager.LoadScene (scene);
+	}
+
+	public void exit(){
+		Application.Quit();
+	}
+}
