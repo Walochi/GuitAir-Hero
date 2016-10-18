1058d15c22c1c85c3d1304dabc397be1c1cce057
Val Vinuya <valvinuya@gmail.com>
2016-10-18 10:10:41 +0800

    no message

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -3,8 +3,27 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour {
+	public GameObject song1;
+	public GameObject song2;
+	public GameObject song3;
+	public GameObject play;
+	public GameObject exitb;
+
+
+	public void Start(){
+		
+	}
+
+	public void show(){			
+		song1.SetActive (true);		
+		song2.SetActive (true);		
+		song3.SetActive (true);
+		play.SetActive (false);
+		exitb.SetActive(false);
+	}
 
 	public void loadLevel(string scene){
+		Debug.Log (scene);
 		SceneManager.LoadScene (scene);
 	}
 
